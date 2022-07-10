a99c651ccfa639f0e99fedc3ee9cd410b54245be
Oscar Santos <oscar303data@gmail.com>
2022-07-09 20:42:10 -0600

    create parks facade

---
+++ app/facades/parks_facade.rb
@@ -0,0 +1,13 @@
+class ParksFacade
+  def self.get_list(state)
+    json = ParksService.get_list_of_parks(state)
+
+    json[:data].map do |park|
+      Park.new(park)
+    end
+  end
+
+  def self.parks_count(state)
+    ParksService.get_list_of_parks(state)[:total]
+  end
+end
