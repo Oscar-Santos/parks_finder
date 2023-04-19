41e659a69356a550b123dfdc1142c2b8287fd618
Oscar Santos <oscar303data@gmail.com>
2023-04-19 15:02:04 -0600

    application yml is created to insert our api key

--- app/services/parks_service.rb
+++ app/services/parks_service.rb
@@ -1,5 +1,15 @@
 class ParksService
     def self.get_list_of_parks(state)
+    end_point = '/api/v1/parks'
+        response = connection.get(end_point) do |faraday|
+            faraday.params['api_key'] = ENV['parks_api_key']
+            faraday.params['stateCode'] = state
+        end
+        JSON.parse(response.body, symbolize_names: true)
+    end
 
+    def self.connection
+        url = 'https://developer.nps.gov'
+        Faraday.new(url: url)
     end
 end
