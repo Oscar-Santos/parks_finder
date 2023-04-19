571d52d558c2e238b658069a3fec9f33334f0f0a
Oscar Santos <oscar303data@gmail.com>
2023-04-19 15:08:23 -0600

    set up jason[:data] on the facades

--- app/facades/parks_facade.rb
+++ app/facades/parks_facade.rb
@@ -1,5 +1,14 @@
 class ParksFacade
     def self.get_list(state)
-        ParksService.get_list_of_parks(state)
+        json = ParksService.get_list_of_parks(state)
+
+        json[:data].map do |park|
+            Park.new(parks)
+        end
     end
 end
+
+# - full name of the park
+# - description
+# - direction info
+# - standard hours for operation
