1eb74a08fda97570f4123c53b1dbf5ab99d07ca7
Oscar Santos <oscar303data@gmail.com>
2023-05-02 10:56:51 -0600

    create a file index_spec to write test for the user story

---
+++ spec/features/parks/index_spec.rb
@@ -0,0 +1,19 @@
+# As a user,
+# When I select "Tennessee" from the form,
+# (Note: use the existing form)
+# And click on "Find Parks",
+# I see the total of parks found,
+# And for each park I see:
+# - full name of the park
+# - description
+# - direction info
+# - standard hours for operation
+
+RSpec.describe Park do
+    describe 'List of National Parks' do
+        it 'returns a list of national parks' do
+            
+        end
+    end
+
+end
