f857364342460e3d7e98b11d8cb3a03240a084b1
Oscar Santos <oscar303data@gmail.com>
2023-05-02 12:09:31 -0600

    both tests: list of parks and count of parks pass the tests

--- spec/facades/parks_facade_spec.rb
+++ spec/facades/parks_facade_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ParksFacade do
+   it 'can return a list of parks' do
+    parks = ParksFacade.get_list('CO')
+    
+    parks.each do |park|
+        expect(park).to be_a(Park)
+    end
+   end
 
+   it 'can find the number of parks' do
+    count_parks = ParksFacade.parks_count('CO')
+    
+    expect(count_parks).to be_a(String)
+    expect(count_parks.to_i).to be > 0
+   end
 end
