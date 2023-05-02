12b83fe0b4c9468e4259de33d0b1d030a13c08f7
Oscar Santos <oscar303data@gmail.com>
2023-05-02 11:10:17 -0600

    parks index spec pass the test

--- spec/features/parks/index_spec.rb
+++ spec/features/parks/index_spec.rb
@@ -8,11 +8,25 @@
 # - description
 # - direction info
 # - standard hours for operation
+require 'rails_helper'
 
 RSpec.describe Park do
     describe 'List of National Parks' do
         it 'returns a list of national parks' do
-            park = ParksFacade.find_parks('TN').first
+            park = ParksFacade.get_list('TN').first
+            parks_count = ParksFacade.parks_count('TN')
+
+            visit root_path
+
+            select 'Tennessee', from: 'state'
+
+            click_on 'Find Parks'
+            expect(page).to have_content(park.name)
+            expect(page).to have_content(park.description)
+            expect(page).to have_content(park.directions)
+            expect(page).to have_content(park.hours)
+
+            expect(page).to have_content(parks_count)
         end
     end
 end
